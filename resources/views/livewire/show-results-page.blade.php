55cf522e77cf1489d88903f3a0c4a0df8c85ee37
Mike <michael.k@gtgroup.io>
2025-06-12 12:15:42 +0300

    Start over from results

--- resources/views/livewire/show-results-page.blade.php
+++ resources/views/livewire/show-results-page.blade.php
@@ -1,5 +1,25 @@
 <div class="px-40 flex flex-1 justify-center py-5">
+
+
+
     <div class="layout-content-container flex flex-col max-w-[960px] flex-1">
+
+
+        <div class="flex px-4 py-3 justify-center">
+            <button
+                class="flex min-w-[84px] max-w-[480px] cursor-pointer items-center justify-center overflow-hidden rounded-xl h-10 px-4 bg-[#eaedf1] text-[#101418] gap-2 pl-4 text-sm font-bold leading-normal tracking-[0.015em]"
+            >
+                <div class="text-[#101418]" data-icon="ArrowLeft" data-size="20px" data-weight="regular">
+                    <svg xmlns="http://www.w3.org/2000/svg" width="20px" height="20px" fill="currentColor" viewBox="0 0 256 256">
+                        <path
+                            d="M224,128a8,8,0,0,1-8,8H59.31l58.35,58.34a8,8,0,0,1-11.32,11.32l-72-72a8,8,0,0,1,0-11.32l72-72a8,8,0,0,1,11.32,11.32L59.31,120H216A8,8,0,0,1,224,128Z"
+                        ></path>
+                    </svg>
+                </div>
+                <a  href='/' class="truncate">Start Over</a>
+            </button>
+        </div>
+
         <div class="flex flex-wrap justify-between gap-3 p-4">
             <div class="flex min-w-72 flex-col gap-3">
                 <p class="text-[#0d141c] tracking-light text-[32px] font-bold leading-tight">Results</p>
